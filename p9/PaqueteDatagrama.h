faad5a22b1b43d5ac940f1d5a3207a685727e59b
ItzelArelyOB <45792918+ItzelArelyOB@users.noreply.github.com>
2020-03-24 18:58:44 -0600

    Practica 9 de socket udp

---
+++ p9/PaqueteDatagrama.h
@@ -0,0 +1,33 @@
+#ifndef PAQUETE_DATAGRAMA_H_
+#define PAQUETE_DATAGRAMA_H_
+#include <new>
+#include <cstring>
+#include <stdio.h>
+#include <cstring>
+#include <arpa/inet.h>
+using namespace std;
+
+class PaqueteDatagrama {
+	public:
+	PaqueteDatagrama(char *, unsigned int, char *, int );
+	PaqueteDatagrama(unsigned int );
+	~PaqueteDatagrama();
+	char *obtieneDireccion();
+	unsigned int obtieneLongitud();
+	int obtienePuerto();
+	char *obtieneDatos();
+	void inicializaPuerto(int);
+	void inicializaIp(char *);
+	void inicializaDatos(char *);
+	private:
+	char *datos;
+	//Almacena los datos
+	char ip[16];
+	//Almacena la IP
+	unsigned int longitud;
+	//Almacena la longitud de de la cadena de datos
+	int puerto;
+	//Almacena el puerto
+};
+
+#endif
---
+++ p9/cliente.cpp
@@ -0,0 +1,29 @@
+#include "SocketDatagrama.h"
+#include<iostream>
+#include<new>
+#include<string>
+
+int main(int argc, char *argv[]) {
+    //Conexión del cliente
+    SocketDatagrama cliente(7777); //Creamos un objeto cliente de tipo Socket Datagram
+    
+    int num[2], res;
+    num[0]=2;
+    num[1]=5;
+    
+    PaqueteDatagrama pack((char *)num, 2*sizeof(int), (char *) argv[1], 7200);
+    printf("%s\n","Enviado a:" );
+    printf("Direccion: %s Puerto:%d\n", pack.obtieneDireccion(), pack.obtienePuerto());
+    cliente.envia(pack);
+    printf("%s\n", "Caso exitoso, se han enviado los numeros");
+
+    PaqueteDatagrama packResultado(sizeof(int));
+    cliente.recibe(packResultado);
+    printf("%s\n","Recibido de:" );
+    printf("Direccion: %s\n Puerto: %d\n", packResultado.obtieneDireccion(),packResultado.obtienePuerto());
+    memcpy(&res, packResultado.obtieneDatos(), sizeof(int));
+    printf("2 + 5 = %d\n", res);
+    printf("%s\n", "En hora buena, ya tenemos el resultado");
+
+    return 0;
+}
