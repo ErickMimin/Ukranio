7c0a44ac66f44891c88c9f2d9b7ea03b093ef083
ItzelArelyOB <itzelarit16@gmail.com>
2020-03-27 17:36:40 -0600

    Se agrega las n solicitudes en la línea de comandos

--- Servidor-Respuesta/client.cpp
+++ Servidor-Respuesta/client.cpp
@@ -12,12 +12,14 @@ int main(int argc, char* argv[]) {
     /* Guardamos numeros en el arreglo */
     nums[0] = atoi(argv[3]);
     nums[1] = atoi(argv[4]);
+    for(int i=0; i<atoi(argv[5]);i++){
+
 
 	Request r;
 	size_t len_reply;
 	int *suma = (int*)r.doOperation(argv[1], (uint16_t) atoi(argv[2]), Menssage::allowedOperations::sum, (char*)nums, sizeof(nums), len_reply);
 	
 	cout << "El resultado de la suma es " << "(longitud = " << len_reply << "): " << *suma << endl;;
-	
+	}
 	return 0;
 }
