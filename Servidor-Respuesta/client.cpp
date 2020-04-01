a4067c06418c80e3865aacf4cad8f6229a52bd7b
@iosuna <iosuna@ipn.mx>
2020-04-01 15:32:44 -0600

    avance practica banco

--- Servidor-Respuesta/client.cpp
+++ Servidor-Respuesta/client.cpp
@@ -7,17 +7,22 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	string ip;
 	uint16_t puerto;
-	int nums[2];
+	int n= atoi(argv[3]);
 
-    /* Guardamos numeros en el arreglo */
+    /* Guardamos numeros en el arreglo 
     nums[0] = atoi(argv[3]);
     nums[1] = atoi(argv[4]);
-
+	*/
+	do{
 	Request r;
 	size_t len_reply;
+	//enviar al servidor el ID
 	int *suma = (int*)r.doOperation(argv[1], (uint16_t) atoi(argv[2]), Menssage::allowedOperations::sum, (char*)nums, sizeof(nums), len_reply);
 	
 	cout << "El resultado de la suma es " << "(longitud = " << len_reply << "): " << *suma << endl;;
+	n--;
+	}while(n>0);
+	
 	
 	return 0;
 }
