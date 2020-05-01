80dc11b471c85dd99fa585dcafae846a192ec15b
ItzelArelyOB <itzelarit16@gmail.com>
2020-05-01 16:51:43 -0500

    Se acomoda ejercicio 1 y ejercicio 2

---
+++ 27_Abril/ReconocimientoV1/Ejercicio1/server.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <fcntl.h>
+#include <unistd.h>
+#include "Response.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    /* Revisamos la entrada del puerto 
+    if(argc < 2){
+        cerr << "ERROR: No name file" << endl;
+        exit(1);
+    }*/
+
+    Response r(3000);
+    map<string, int> nbd;
+
+    cout << "Servidor iniciado..." << endl;
+    /*int destino = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 0666);*/
+
+    while(true){
+        Menssage *msg = r.getRequest();
+        //int* nums = (int*)msg->arguments;
+        /*destino = open(argv[1], O_APPEND|O_WRONLY, 0666);*/
+
+        cout << "Solicitud enviada desde " << r.address << ":" << r.port << endl;
+        cout << " request ID = " << msg->requestId << endl;
+
+        if(msg->operationId == Menssage::allowedOperations::send){
+            Registro *regis = (Registro*)msg->arguments;
+            cout << regis->celular << endl;
+            cout << regis->CURP << endl;
+            cout << regis->partido << endl;
+            int response= 1;
+            /*int response = write(destino, regis, sizeof(Registro));*/
+            r.sendResponse((char*)& response, sizeof(int));
+            /*close(destino);*/
+        }
+            
+        
+    }
+    cout << "Servivor apagandose..." << endl;
+    return 0;
+}
+
+//|O_APPEND|O_CREAT|O_TRUNC
