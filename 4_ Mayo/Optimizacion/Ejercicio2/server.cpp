87e228f3a7dfa783f96f44209474cfbdb0925587
@iosuna <iosuna@ipn.mx>
2020-05-05 22:37:54 -0500

    Se casi termina practica de optimización

--- 4_ Mayo/Optimizacion/Ejercicio2/server.cpp
+++ 4_ Mayo/Optimizacion/Ejercicio2/server.cpp
@@ -12,13 +12,77 @@
 
 using namespace std;
 
+const int ALPHABET_SIZE = 26; 
+  
+// trie node 
+struct TrieNode 
+{ 
+    struct TrieNode *children[ALPHABET_SIZE]; 
+  
+    // isEndOfWord is true if the node represents 
+    // end of a word 
+    bool isEndOfWord; 
+}; 
+  
+// Returns new trie node (initialized to NULLs) 
+struct TrieNode *getNode(void) 
+{ 
+    struct TrieNode *pNode =  new TrieNode; 
+  
+    pNode->isEndOfWord = false; 
+  
+    for (int i = 0; i < ALPHABET_SIZE; i++) 
+        pNode->children[i] = NULL; 
+  
+    return pNode; 
+} 
+  
+// If not present, inserts key into trie 
+// If the key is prefix of trie node, just 
+// marks leaf node 
+void insert(struct TrieNode *root, string key) 
+{ 
+    struct TrieNode *pCrawl = root; 
+  
+    for (int i = 0; i < key.length(); i++) 
+    { 
+        int index = key[i] - 'a'; 
+        if (!pCrawl->children[index]) 
+            pCrawl->children[index] = getNode(); 
+  
+        pCrawl = pCrawl->children[index]; 
+    } 
+  
+    // mark last node as leaf 
+    pCrawl->isEndOfWord = true; 
+} 
+  
+// Returns true if key presents in trie, else 
+// false 
+bool search(struct TrieNode *root, string key) 
+{ 
+    struct TrieNode *pCrawl = root; 
+  
+    for (int i = 0; i < key.length(); i++) 
+    { 
+        int index = key[i] - 'a'; 
+        if (!pCrawl->children[index]) 
+            return false; 
+  
+        pCrawl = pCrawl->children[index]; 
+    } 
+  
+    return (pCrawl != NULL && pCrawl->isEndOfWord); 
+} 
+
+
 int main(int argc, char* argv[]){
     /* Revisamos la entrada del puerto */
     if(argc < 2){
         //cerr << "ERROR: No name file" << endl;
         exit(1);
     }
-
+    struct TrieNode *root = getNode(); 
     Response r(3000);
     map<string, int> nbd;
 
@@ -42,14 +106,15 @@ int main(int argc, char* argv[]){
             
             Registro *regis = (Registro*)msg->arguments;
             
-            // using default comparison:
-            std::sort (v.begin(), v.end());
-            if (std::binary_search (v.begin(), v.end(),regis->celular)){
+            
+            
+            if (search(root, string(regis->celular))){
                 //std::cout << "Numero Repetido!\n";
                 snprintf(buffer,16,"%du%d", 0,0); // se concatena los segundos con microsegundo
                 r.sendResponse(buffer, strlen(buffer)); 
             }
             else {
+                insert(root, string(regis->celular));
                 //std::cout << "Numero NO Repetido.\n";
                 memcpy(regis->sec, buffer, 16);
                 /*cout << regis->celular << endl;
