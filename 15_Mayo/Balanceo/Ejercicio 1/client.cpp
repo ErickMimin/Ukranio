f4c64a12f83d8e102ec5ea308f58a0c6e285069a
ItzelArelyOB <itzelarit16@gmail.com>
2020-05-15 18:26:57 -0500

    Avances

--- 15_Mayo/Balanceo/Ejercicio 1/client.cpp
+++ 15_Mayo/Balanceo/Ejercicio 1/client.cpp
@@ -18,6 +18,7 @@ Request REQUESTs[3];
 
 void Send(string ip, uint16_t port, string BUFFER, int rqN){
 	size_t len_response;
+	cout <<BUFFER << endl;
 	REQUESTs[rqN].doOperation(ip, (uint16_t) port, Menssage::allowedOperations::send, (char*)BUFFER.c_str(), TAM_MAX_DATA, len_response);
 }
 
