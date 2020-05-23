17d8f4f9860c0366c91299a80cd1d56cbe45e085
ErickMimin <Eri_mimo@hotmail.com>
2020-05-23 00:23:43 -0500

    [Add] Practica ejemplo mongoose

--- 18_Mayo/MONGOOSE/mongoose_REST/server.cpp
+++ 18_Mayo/MONGOOSE/mongoose_REST/server.cpp
@@ -1,23 +1,39 @@
 #define MG_ENABLE_HTTP_STREAMING_MULTIPART 1
 #include "mongoose.h"
+#include "DatagramSocket.h"
+#include "DatagramPacket.h"
 #include <stdio.h>
 #include <iostream>
-
+#include <sys/time.h>
+#include <thread>
 using namespace std;
 
-static const char *s_http_port = "8000";
+static const char *s_http_port ;
 static struct mg_serve_http_opts s_http_server_opts;
+struct timeval tv1, tv2;
 
-
-static void handle_size(struct mg_connection *nc, struct http_message *hm) {
+static void handle_http(struct mg_connection *nc, struct http_message *hm, DatagramSocket &s) {
 		char query[256];
-
-		mg_get_http_var(&hm->body, "query", query,sizeof(query));
-		sprintf(query, "Longitud de la cadena = %d caracteres", (int)strlen(query));
-		printf("Cadena enviada: %s\n", query);
-
-		mg_send_head(nc,200,strlen(query), "Content-Type: text/plain");
-		mg_printf(nc, "%s", query);
+		string response;
+		size_t len_response;
+		mg_get_http_var(&hm->body, "query", query, sizeof(query));
+		printf("Address: %s\n", query);
+		gettimeofday(&tv1, NULL);
+		int servers = 100, r, num;
+		DatagramPacket pqres((char*) &num, sizeof(num));
+		while(servers--){
+			r = s.receiveTimeout(pqres, 0, 500000);
+			if(r >= 0){
+				gettimeofday(&tv2, NULL);
+				response.append(pqres.getAddress());
+				response.append(", Time: ");
+				response.append(to_string(tv2.tv_usec - tv1.tv_usec));
+				response.append("</br>");
+			}
+		}
+		
+		mg_send_head(nc, 200, response.size(), "Content-Type: text/plain");
+		mg_printf(nc, "%s", response.c_str());
 }
 
 static void ev_handler(struct mg_connection *nc, int ev, void *p) {
@@ -30,8 +46,10 @@ static void ev_handler(struct mg_connection *nc, int ev, void *p) {
 			
 			mg_get_http_var(&hm->body, "query", query,sizeof(query));
 			printf("Cadena introducida: %s\n",query);
-
-		    handle_size(nc, hm);  
+			DatagramPacket pq(NULL, 0, query, 7200);
+			DatagramSocket sock(7200);
+			sock.send(pq);
+		    handle_http(nc, hm, sock);  
 		}else{
 			mg_serve_http(nc, (struct http_message *) p, s_http_server_opts);
 		}
@@ -39,9 +57,10 @@ static void ev_handler(struct mg_connection *nc, int ev, void *p) {
 
 }
 
-int main(void) {
+int main(int argc, char* argv[]) {
 	struct mg_mgr mgr;
 	struct mg_connection *nc;
+	s_http_port = (const char *)argv[1];
 	mg_mgr_init(&mgr, NULL);
 
 	printf("Starting web server on port %s\n", s_http_port);
